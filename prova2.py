7a125ffac25061400b5fedb28a71028660c23307
PP90 <pietropiscione@gmail.com>
2015-12-09 17:29:31 +0100

    Added function to get the gateway address. It actually doesn't work, but it will be used because in order to make the SNMP request the manager has to know the gateway address.

--- prova2.py
+++ prova2.py
@@ -1,5 +1,38 @@
 from pysnmp.hlapi import *
 import time
+import socket, struct
+
+def get_if_speed():
+	errorIndication, errorStatus, errorIndex, varBinds = next(
+	    getCmd(SnmpEngine(),
+        	   CommunityData('public', mpModel=0),
+        	   UdpTransportTarget(('192.168.3.1', 161)),#Address and port
+        	   ContextData(),
+		  		ObjectType(ObjectIdentity("1.3.6.1.2.1.2.2.1.5.1"))#IfSpeed
+		 	 )
+	)
+
+	if errorIndication:
+  		print(errorIndication)
+	elif errorStatus:
+		print('%s at %s' % (
+			errorStatus.prettyPrint(),
+			errorIndex and varBinds[int(errorIndex)-1][0] or '?'
+        	)
+    	)
+			
+	ifSpeed=varBinds[0].prettyPrint().split("= ",1)[1];
+	return ifSpeed
+
+##Actually doesn't give the gateway address. To fix
+def get_default_gateway_linux():
+	with open("/proc/net/route") as fh:
+		for line in fh:
+			fields = line.strip().split()
+			if fields[1] != '00000000' or not int (fields[3], 16) & 2:
+				continue
+
+	return socket.inet_ntoa(struct.pack("<L", int(fields[2],16)))
 
 ##In caso di errore ritornare -1
 def get_if_speed():
@@ -26,10 +59,12 @@ def get_if_speed():
 
 
 if_speed=float(get_if_speed());
+print 'Gateway address:', get_default_gateway_linux();
 old_ifInBytes=0;
 old_ifOutBytes=0;
 old_TimeUp=0;
 how_much_often=1;
+
 while True:
 	errorIndication, errorStatus, errorIndex, varBinds = next(
 	    getCmd(SnmpEngine(),
