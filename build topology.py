bb1f22264c4eff5da2f09f0d3435d314dc8e9b37
luigiDB <luigi.debianchi@gmail.com>
2015-12-24 01:02:16 +0100

    Build the topology with quagga. The code is still error prone since no error handling routines are present

--- build topology.py
+++ build topology.py
@@ -1,6 +1,7 @@
 import subprocess
 #import ipaddress
 import re
+import pprint
 
 def decodeTopology(output):
     #variabili
@@ -35,7 +36,7 @@ def decodeTopology(output):
     #print(startRow)
 
     #we only need the first ip of each line
-    #search for "ip ip " until empty line
+    #search for "ip" until empty line
     for i in range(startRow, len(rows)):
         if(len(rows[i]) == 0):
             #empty line found
@@ -48,8 +49,60 @@ def decodeTopology(output):
 
 
 
+def findDr(draddress, listInfo, originalNode):
+    for i in range(0, len(listInfo)):
+        routes = listInfo[i]['nRoutes']
+        for j in range(0, routes):
+            if(listInfo[i][str(j) + '_draddress'] == draddress):
+                if(i != originalNode):
+                    return i
+    return -1
+
+
+
+def buildTopologyMatrix(interfaces):
+    listInfo = []
+    topologyMatrix = [[0 for x in range(len(interfaces))] for x in range(len(interfaces))]
+
+    ruleIP = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+
+    for i in interfaces:
+        listInfo.append({})
+        listInfo[len(listInfo) - 1]['routerId'] = i
+        cmd = 'vtysh -c "show ip ospf database router ' + i + '"'
+        output = subprocess.check_output(cmd , shell = True)
+        rows = output.split('\n')
+        #pprint.pprint(rows)
+
+        #rules
+        #rule1 = re.compile('Transit Network')
+        #next line is (Link ID) Designated Router address: 10.2.2.2
+        #next line is (Link Data) Router Interface address: 10.2.2.1
+        counter = 0
+        for row in range(0, len(rows)):
+            if(re.search('Link connected to: a Transit Network', rows[row]) is not None):
+                if(re.search('\(Link ID\) Designated Router address:', rows[row + 1]) is not None):
+                    listInfo[len(listInfo) - 1][str(counter) + '_draddress'] = ruleIP.search(rows[row + 1]).group()
+                if(re.search('\(Link Data\) Router Interface address:', rows[row + 2]) is not None):
+                    listInfo[len(listInfo) - 1][str(counter) + '_ip'] = ruleIP.search(rows[row + 2]).group()
+                counter = counter + 1
+        listInfo[len(listInfo) - 1]['nRoutes'] = counter
+
+    #pprint.pprint(listInfo)
+    for i in range(0, len(listInfo)):
+        routes = listInfo[i]['nRoutes']
+        for j in range(0, routes):
+            draddress = listInfo[i][str(j) + '_draddress']
+            k = findDr(draddress, listInfo, i)
+            if(k < 0):
+                print 'ERROR'
+            topologyMatrix[i][k] = listInfo[i][str(j) + '_ip']
+    return topologyMatrix
+
+
 
 output = subprocess.check_output('vtysh -c "show ip ospf database"', shell = True)
-decodeTopology(output)
-
-
+interfaces = decodeTopology(output)
+print(interfaces)
+matrix = buildTopologyMatrix(interfaces)
+print(matrix)
