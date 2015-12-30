1a6b289ec5964e8afc22087a5c726160b2eb3f6e
BigMikes <giulio.micheloni@gmail.com>
2015-12-30 23:10:58 +0100

    Added utility functions to this class

--- Mpls_snmp/Container.py
+++ Mpls_snmp/Container.py
@@ -4,8 +4,6 @@ class Container(object):
 		self._name = name
 		self._info = {}
 	
-	def getName(self):
-		return self._name
 	
 	"""It adds a new entry in the dictionary '_info' and it returns the inserted value, if it is empty, 
 	otherwise it returns the current value"""
@@ -21,4 +19,20 @@ class Container(object):
 		for elem in sorted(self._info.keys()):
 			returnString += '\t' + elem + ": " + str(self._info[elem]) + '\n'
 		return returnString
+	
+	def getName(self):
+		return self._name
+		
+	"""This function returns the value of a given attribute passed as parameter <attrName>"""
+	"""if there isn't a attribute named as <attrName>, it returns None"""
+	def getAttribute(self, attrName):
+		if attrName not in self._info.keys():
+			return None
+		return self._info[attrName]
+		
+	"""This function returns the entire dictionary of attribute"""
+	"""The dictionary is structured as follow: {'Attr1' : val1, 'Attr2' : val2, ...}"""
+	def getAttributeDict(self):
+		return self._info
+		
 	
