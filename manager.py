4b87fcff5e0a970869383cc13157590c42c53df1
luigiDB <luigi.debianchi@gmail.com>
2016-01-23 18:02:17 +0100

    + Support for utlization given ip address of the router + additional information handled in class

--- manager.py
+++ manager.py
@@ -1,6 +1,7 @@
 import buildTopology
+#import Mpls_snmp
+from SNMP_utilization_src import *
 
-import Mpls_snmp
 #####################################
 #find method are for private use
 #get method are for public call
@@ -42,28 +43,43 @@ class Manager:
         return listIp
 
     def getTopology(self):
-
-
-    ###############UTILIZATION
-    #def findUtilization():
-
-
-    #def getUtilization():
-    #def getAllUtilization():
-
-
         """return topology if not present call function to populate data"""
         if self.topologyMatrix is None:
             #unallocated memory call find function
             self.findTopology()
         return self.topologyMatrix
 
-    ###############OCCUPATION
-    #def findOccuption(ip):
+    ###############UTILIZATION
+    def findUtilization(self, addr):
+        """if the output is None some error occurred"""
+        index = None
+        for k in range(0, len(self.listInfo)):
+            if self.listInfo[k]['routerId'] == addr:
+                index = k
+        if index is None:
+            return None
+        #print(index)
+        a = []
+        a.append(addr)
+        r = getRouterInfo.get_routers_list(a, self.communityString)
+        ifs = r[0].get_interfaces()
+        for i in range(0, len(ifs)):
+            ip = ifs[i].get_address_if()
+            for j in range(0, self.listInfo[index]['nRoutes']):
+                if ip == self.listInfo[index][str(j) + '_ip']:
+                    #match
+                    #add new info
+                    self.listInfo[index][str(j) + '_name'] = ifs[i].get_name()
+                    self.listInfo[index][str(j) + '_id'] = ifs[i].get_id()
+                    self.listInfo[index][str(j) + '_speed'] = ifs[i].get_if_speed()
+                    self.listInfo[index][str(j) + '_utilization'] = ifs[i].get_in_out_utilization()
 
-    #def getOccupation(ip, interface):
 
-    #def getAllOccupation():
+
+
+    #def getUtilization():
+    #def getAllUtilization():
+
 
     ###############TUNNEL
     #def findTunnel(ip, communityString):
