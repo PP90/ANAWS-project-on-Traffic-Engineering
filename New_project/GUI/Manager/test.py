fdc22cacfa479746905bec5e9d571479c4d1b529
luigiDB <luigi.debianchi@gmail.com>
2016-01-25 18:09:00 +0100

    test file

--- New_project/GUI/Manager/test.py
+++ New_project/GUI/Manager/test.py
@@ -3,9 +3,9 @@ import pprint
 
 m1 = man.Manager("192.168.3.1", "public")
 
-a = m1.getTopology()
-print("\n\n matrix")
-pprint.pprint(a)
+#a = m1.getTopology()
+#print("\n\n matrix")
+#pprint.pprint(a)
 
 #a = m1.getListIP()
 #print("\n\n list")
@@ -19,9 +19,9 @@ a = m1.getUtilization("1.1.1.1")
 print("\n\n result")
 pprint.pprint(a)
 
-#a = m1.listInfo
-#print("\n\n listInfo")
-#pprint.pprint(a)
+a = m1.listInfo
+print("\n\n listInfo")
+pprint.pprint(a)
 
 #a = m1.getRoutersList(["1.1.1.1"])
 #print("\n\n getRoutersList")
